d7491556b171f3d56c479549bf7f913e0cafde3d
Jagan Karunakaran <jagan.karan13@gmail.com>
2020-07-15 23:30:29 +0530

    Added Confluent Kafka Support

--- app/main_screen.py
+++ app/main_screen.py
@@ -14,6 +14,7 @@ class MainScreen(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.product = Constants.APACHE_KAFKA
         self.open_file_text_field = QLineEdit()
         self.application_image = QLabel(self)
         self.setWindowTitle(Constants.APPLICATION_WINDOW_TITLE)
@@ -38,6 +39,27 @@ class MainScreen(QWidget):
         header_wrapper.addWidget(self.application_image)
         header_wrapper.addSpacing(30)
 
+        # Choose File Wrapper
+        version_wrapper = QVBoxLayout()
+
+        file_wrapper = QVBoxLayout()
+        radio_wrapper = QHBoxLayout()
+
+        apache_kafka = QRadioButton(Constants.APACHE_KAFKA)
+        apache_kafka.setChecked(True)
+        apache_kafka.toggled.connect(lambda: self.productCheckedInRadioButton(apache_kafka))
+        confluent_kafka = QRadioButton(Constants.CONFLUENT_KAFKA)
+        choose_folder_button = QPushButton(Constants.CHOOSE_FOLDER)
+        choose_folder_button.clicked.connect(self.chooseFile)
+        radio_wrapper.addWidget(apache_kafka)
+        radio_wrapper.addWidget(confluent_kafka)
+        file_wrapper.addWidget(self.open_file_text_field)
+        file_wrapper.addWidget(choose_folder_button)
+
+        version_wrapper.addLayout(radio_wrapper)
+        version_wrapper.addLayout(file_wrapper)
+        version_wrapper.setContentsMargins(100, 10, 20, 30)
+
         # Start and Stop Button Content wrapper.
         button_wrapper = QVBoxLayout()
         start_button = QPushButton(Constants.START_BUTTON)
@@ -50,17 +72,9 @@ class MainScreen(QWidget):
         button_wrapper.addWidget(stop_button)
         button_wrapper.setContentsMargins(100, 10, 20, 30)
 
-        # Choose File Wrapper
-        file_wrapper = QHBoxLayout()
-        choose_folder_button = QPushButton(Constants.CHOOSE_FOLDER)
-        choose_folder_button.clicked.connect(self.chooseFile)
-        file_wrapper.addWidget(self.open_file_text_field)
-        file_wrapper.addWidget(choose_folder_button)
-        file_wrapper.setContentsMargins(100, 10, 20, 30)
-
         # Content wrapper
         content_wrapper.addLayout(button_wrapper)
-        content_wrapper.addLayout(file_wrapper)
+        content_wrapper.addLayout(version_wrapper)
 
         # Main wrapper
         main_wrapper.addLayout(header_wrapper)
@@ -84,25 +98,28 @@ class MainScreen(QWidget):
         print("Open File")
         file_path = QFileDialog.getExistingDirectory(self, Constants.SELECT_FOLDER, "", QFileDialog.ShowDirsOnly)
         print(file_path)
-        self.open_file_text_field.setText(file_path)
-        self.open_file_text_field.setCursorPosition(0)
-        palette = QPalette()
-        palette.setColor(QPalette.Base, QColor('darkGray'))
-        palette.setColor(QPalette.Text, QColor('black'))
-        self.open_file_text_field.setPalette(palette)
-        self.open_file_text_field.setReadOnly(True)
+        is_file_path_empty = self.validateTextFieldEmpty(file_path)
+        if not is_file_path_empty:
+            self.open_file_text_field.setText(file_path)
+            self.open_file_text_field.setCursorPosition(0)
+            palette = QPalette()
+            palette.setColor(QPalette.Base, QColor('darkGray'))
+            palette.setColor(QPalette.Text, QColor('black'))
+            self.open_file_text_field.setPalette(palette)
+            self.open_file_text_field.setReadOnly(True)
 
     """Start the Kafka and Zookeeper Server"""
 
     def startkafka(self):
         print("startKafka")
         kafka_file_path = self.open_file_text_field.text()
-        is_text_field_empty = self.validateTextFieldNotEmpty(kafka_file_path)
+        is_text_field_empty = self.validateTextFieldEmpty(kafka_file_path)
+        zookeeper_config, kafka_config = self.configPathBasedOnProduct(self.product)
         if not is_text_field_empty:
-            subprocess.Popen(["sh", Constants.ZOOKEEPER_START_SERVER_PATH, Constants.ZOOKEEPER_START_SERVER_CONFIG],
+            subprocess.Popen(["sh", Constants.ZOOKEEPER_START_SERVER_PATH, zookeeper_config],
                              cwd=kafka_file_path)
             time.sleep(3)
-            subprocess.Popen(["sh", Constants.KAFKA_START_SERVER_PATH, Constants.KAFKA_START_SERVER_CONFIG],
+            subprocess.Popen(["sh", Constants.KAFKA_START_SERVER_PATH, kafka_config],
                              cwd=kafka_file_path)
         else:
             QMessageBox.information(self, "Alert", "Please select the kafka folder")
@@ -112,7 +129,7 @@ class MainScreen(QWidget):
     def stopKafka(self):
         print("stopKafka")
         kafka_file_path = self.open_file_text_field.text()
-        is_text_field_empty = self.validateTextFieldNotEmpty(kafka_file_path)
+        is_text_field_empty = self.validateTextFieldEmpty(kafka_file_path)
         if is_text_field_empty:
             QMessageBox.information(self, "Alert", "Please select the kafka folder")
         else:
@@ -149,14 +166,28 @@ class MainScreen(QWidget):
         else:
             return False
 
-    def validateTextFieldNotEmpty(self, stringToBeValidated):
+    def validateTextFieldEmpty(self, stringToBeValidated):
         print("validateTextFieldNotEmpty")
-        print(stringToBeValidated)
         if stringToBeValidated != "":
             return False
         else:
             return True
 
+    def configPathBasedOnProduct(self, productName):
+        if productName == Constants.APACHE_KAFKA:
+            return Constants.APACHE_ZOOKEEPER_START_SERVER_CONFIG, Constants.APACHE_KAFKA_START_SERVER_CONFIG
+        elif productName == Constants.CONFLUENT_KAFKA:
+            return Constants.CONFLUENT_ZOOKEEPER_START_SERVER_CONFIG, Constants.CONFLUENT_KAFKA_START_SERVER_CONFIG
+
+    def productCheckedInRadioButton(self, button):
+        if button.text() == Constants.APACHE_KAFKA:
+            if button.isChecked():
+                self.product = Constants.APACHE_KAFKA
+            else:
+                self.product = Constants.CONFLUENT_KAFKA
+        else:
+            self.product = Constants.CONFLUENT_KAFKA
+        print(self.product)
 
 """Main Application"""
 
